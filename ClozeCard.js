cda1ddf66f8a041c3f47d7288a4ba5f91eb4b15b
Danny <DJ@MacBook-Pro.local>
2017-07-25 18:32:20 -0600

    pretty

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,9 +1,10 @@
 module.exports = ClozeCard;
+
 function ClozeCard(text, cloze) {
-	 if (!(this instanceof ClozeCard)) { 
-    
-    return new ClozeCard(text, cloze);
-  } 
-	this.text = text;
-	this.cloze = cloze;
+    if (!(this instanceof ClozeCard)) {
+
+        return new ClozeCard(text, cloze);
+    }
+    this.text = text;
+    this.cloze = cloze;
 }
--- main.js
+++ main.js
@@ -8,18 +8,16 @@ var i = 0;
 var msg;
 var validate;
 
-function cards(){
-if(cardArray[i].partial) {
-	msg = cardArray[i].partial;
-	validate = cardArray[i].cloze;
-}
-else if (cardArray[i].front){
-	msg = cardArray[i].front;
-	validate = cardArray[i].back;
-}
-else{
-	console.log("Trivia game over")
-}
+function cards() {
+    if (cardArray[i].partial) {
+        msg = cardArray[i].partial;
+        validate = cardArray[i].cloze;
+    } else if (cardArray[i].front) {
+        msg = cardArray[i].front;
+        validate = cardArray[i].back;
+    } else {
+        console.log("Trivia game over")
+    }
 }
 //takes answers from terminal 
 
@@ -39,137 +37,131 @@ else{
 
 //takes answers from inquirer
 
-var questions = function(){
-		console.log("new question");
-		inquirer.prompt([{
-			type: "input",
-			name: "number",
-			message: "How many flashcards would you like to create?",
-			validate: function(input){
-				if(isNaN(input) === true){
-					console.log("\r\n Please enter a number");
-					return false
-				}
-				else{
-					return true
-				}
-			}}
-			]).then(function (answers) {
-				console.log(answers.number);
-				numberOfCards = answers.number;
-				createCards();
-				});
-		}
-	
-questions();
-
-var createCards = function(){
-	if(numberOfCards !== 0){
-		cardMaker(); 
-		}
-		else{
-		 trivia();
-		}
-	}
-function cardMaker(){
-	inquirer.prompt([{
-			type: 'list',
-	      	choices: ['Basic Card', 'Cloze Card'],
-	     	name: 'cardChoice',
-	      	message: 'What kind of card would you like to create?\r\n Basic Card: Will be in a simple question/answer format. \r\n Cloze Card: Will use a sentence that has its answer removed.\r\n'
-			}
-			]).then(function (answers) {
-				console.log(answers.cardChoice)
-				if(answers.cardChoice === 'Basic Card'){
-					console.log('You chose to create a basic card.');
-					basic()
-				}
-				else if(answers.cardChoice === 'Cloze Card'){
-					console.log('You chose to create a close card.');
-					cloze()
-				}
-});
-		}
-
-function basic(){
-          inquirer.prompt([
-          {
-          	  type: 'input',
-              name: 'front',
-              message: 'Please enter your question.'
-          },
-          {
-          	  type: 'input',
-              name: 'back',
-              message: 'Please enter your answer.'
-          }
-
-            ]).then(function(secondAnswers){
-            	var newCard = new NewCard(secondAnswers.front, secondAnswers.back);
-            	cardArray.push(newCard);
-            	
-            	createCards();
-				});
-				numberOfCards--;
+var questions = function() {
+    console.log("new question");
+    inquirer.prompt([{
+        type: "input",
+        name: "number",
+        message: "How many flashcards would you like to create?",
+        validate: function(input) {
+            if (isNaN(input) === true) {
+                console.log("\r\n Please enter a number");
+                return false
+            } else {
+                return true
+            }
+        }
+    }]).then(function(answers) {
+        console.log(answers.number);
+        numberOfCards = answers.number;
+        createCards();
+    });
 }
 
-		
-		function cloze(){
-          inquirer.prompt([
-          {
-          	  type: 'input',
-              name: 'front',
-              message: 'Please enter a full sentence that includes answer and question.\r\n Ex. George Washington was the first president.\r\n',
-        
-          },
-          {
-          	  type: 'input',
-              name: 'back',
-              message: 'Please enter your cloze answer portion.\r\n Ex. George Washington.\r\n',
+questions();
 
-          }
+var createCards = function() {
+    if (numberOfCards !== 0) {
+        cardMaker();
+    } else {
+        trivia();
+    }
+}
 
-            ]).then(function(clozeAnswers){
-            	var clozeCard = new ClozeCard(clozeAnswers.front, clozeAnswers.back);
-            	clozeCard.partial = clozeAnswers.front.replace(clozeAnswers.back, " ... ");
-       
-            	cardArray.push(clozeCard);
-        
-            	createCards();
-				});
-				numberOfCards--;
-				
-			}
-	
+function cardMaker() {
+    inquirer.prompt([{
+        type: 'list',
+        choices: ['Basic Card', 'Cloze Card'],
+        name: 'cardChoice',
+        message: 'What kind of card would you like to create?\r\n Basic Card: Will be in a simple question/answer format. \r\n Cloze Card: Will use a sentence that has its answer removed.\r\n'
+    }]).then(function(answers) {
+        console.log(answers.cardChoice)
+        if (answers.cardChoice === 'Basic Card') {
+            console.log('You chose to create a basic card.');
+            basic()
+        } else if (answers.cardChoice === 'Cloze Card') {
+            console.log('You chose to create a close card.');
+            cloze()
+        }
+    });
+}
 
-function trivia(){
-	if(i === 0){
-		console.log("Your flash cards have been created and the triva game has begun.")
-	}
-	if(i < cardArray.length){
-	cards();
-	inquirer.prompt([
-          {
-          	  type: 'input',
-              name: 'front',
-              message: msg,
-              validate: function(input){
-				if(input === validate){
-					console.log("\r\n You are right!");
-					return true
-				}
-				else{
-					console.log("\r\nSorry that is not the answer... Please try again.")
-					return false
-				}
-			}}
-          
-          
+function basic() {
+    inquirer.prompt([{
+            type: 'input',
+            name: 'front',
+            message: 'Please enter your question.'
+        },
+        {
+            type: 'input',
+            name: 'back',
+            message: 'Please enter your answer.'
+        }
 
-            ]).then(function(secondAnswers){
-            	
-            	trivia();
-				});
-				i++;
-			}
+    ]).then(function(secondAnswers) {
+        var newCard = new NewCard(secondAnswers.front, secondAnswers.back);
+        cardArray.push(newCard);
+
+        createCards();
+    });
+    numberOfCards--;
+}
+
+
+function cloze() {
+    inquirer.prompt([{
+            type: 'input',
+            name: 'front',
+            message: 'Please enter a full sentence that includes answer and question.\r\n Ex. George Washington was the first president.\r\n',
+
+        },
+        {
+            type: 'input',
+            name: 'back',
+            message: 'Please enter your cloze answer portion.\r\n Ex. George Washington.\r\n',
+
+        }
+
+    ]).then(function(clozeAnswers) {
+        var clozeCard = new ClozeCard(clozeAnswers.front, clozeAnswers.back);
+        clozeCard.partial = clozeAnswers.front.replace(clozeAnswers.back, " ... ");
+
+        cardArray.push(clozeCard);
+
+        createCards();
+    });
+    numberOfCards--;
+
+}
+
+
+function trivia() {
+    if (i === 0) {
+        console.log("Your flash cards have been created and the triva game has begun.")
+    }
+    if (i < cardArray.length) {
+        cards();
+        inquirer.prompt([{
+                type: 'input',
+                name: 'front',
+                message: msg,
+                validate: function(input) {
+                    if (input === validate) {
+                        console.log("\r\n You are right!");
+                        return true
+                    } else {
+                        console.log("\r\nSorry that is not the answer... Please try again.")
+                        return false
+                    }
+                }
+            }
+
+
+
+        ]).then(function(secondAnswers) {
+
+            trivia();
+        });
+        i++;
+    }
 }
