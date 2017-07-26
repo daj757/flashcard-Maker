3ce271e472ec83f323c4a8ebc9757636a67a8aa5
Danny <DJ@MacBook-Pro.local>
2017-07-25 18:23:06 -0600

    final

--- BasicCard.js
+++ BasicCard.js
@@ -1,5 +1,9 @@
 module.exports = NewCard;
 function NewCard (front, back) {
+	if (!(this instanceof NewCard)) { 
+    
+    return new NewCard(front, back);
+  } 
 	this.front = front;
 	this.back = back;
 
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,5 +1,9 @@
 module.exports = ClozeCard;
 function ClozeCard(text, cloze) {
+	 if (!(this instanceof ClozeCard)) { 
+    
+    return new ClozeCard(text, cloze);
+  } 
 	this.text = text;
 	this.cloze = cloze;
 }
